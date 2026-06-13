cb4ee0ca807a985c5c13edb2ef0b5387d3727787
Prince benrich <benrichprince1@gmail.com>
2026-06-13 20:04:01 +0100

    nodejs

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/hello", (req, res) => {
+  res.json({message: "hello world"});
+});
+
+ 
 const PORT = 5001;
 const server = app.listen(PORT, () => {console.log(`Server running on PORT ${PORT}`);});
 

42eecc570e558a05d4fdb4c61459c4da0976b890
Prince benrich <benrichprince1@gmail.com>
2026-06-17 20:43:23 +0100

    movie routes

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -2,21 +2,21 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {res.json({message: "hello"});
+router.get("/", (req, res) => {res.json({httpMethod: "get"});
 
 });
 
 
-router.post("/", (req, res) => {res.json({message: "hello"});
+router.post("/", (req, res) => {res.json({httpMethod: "post"});
 
 });
 
-router.put("/", (req, res) => {res.json({message: "hello"});
+router.put("/", (req, res) => {res.json({httpMethod: "put"});
 
 });
 
 
-router.delete("/", (req, res) => {res.json({message: "hello"});
+router.delete("/", (req, res) => {res.json({httpMethod: "delete"});
 
 });
 
